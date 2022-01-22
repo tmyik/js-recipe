8b57dde2123c7949a8755093642180062cf857cd
tmyik <tenitm43@outlook.jp>
2022-01-22 15:44:22 +0900

    [■と〇]を学習

---
+++ 02_レシピ/01_□と○/main.js
@@ -0,0 +1,11 @@
+const figure = document.getElementById("figure")
+
+figure.onclick = function () {
+  //figureにroundedクラスを追加
+  figure.classList.toggle("rounded")
+}
+
+// squareButton.onclick = function () {
+//   //figureからroundedクラスを削除
+//   figure.classList.remove("rounded")
+// }
