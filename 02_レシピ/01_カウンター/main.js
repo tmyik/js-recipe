9e2b6c3bc4c79c4bb9dbb332f09d2bb51a789546
tmyik <tenitm43@outlook.jp>
2022-01-23 18:19:06 +0900

    [カウンター発展2]の学習

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,9 +1,23 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const twiceButton = document.getElementById("twice-button")
 
 let count = 0
 
 plusButton.onclick = function () {
   count++
+  display.textContent = count
+}
+
+minusButton.onclick = function () {
+  if (count != 0) {
+    count--
+    display.textContent = count
+  }
+}
+
+twiceButton.onclick = function () {
+  count += count
   display.textContent = count
 }
