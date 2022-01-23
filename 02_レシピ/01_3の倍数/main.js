db536c7c52ab36df99542079a40ff30578af674b
tmyik <tenitm43@outlook.jp>
2022-01-23 17:53:35 +0900

    [3の倍数 発展2]の学習

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,9 +1,18 @@
 const numCount = function (number) {
-  for (let i; i <= number; i++) {
-    if (i % 3 === 0) {
-      console.log(`${i}!!!!!!!`)
+  for (let i = 1; i <= number; i++) {
+    // let char = i + ""
+    if (i % 15 === 0) {
+      console.log("FizzBuzz")
+    } else if (i % 5 === 0) {
+      console.log("Buzz")
+    } else if (i % 3 === 0) {
+      console.log("Fizz")
+    } else if (String(i).includes("3")) {
+      console.log("3が入っちゃう元気")
     } else {
       console.log(i)
     }
   }
 }
+
+numCount(40)
